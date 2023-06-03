c2e39383bf80dbd66893cdf2810ee8b41412229f
Ian Galvan <ian.salcedo@hotmail.com>
2023-06-03 17:22:13 -0300

    agregando libreria

--- js/carrito.js
+++ js/carrito.js
@@ -1,49 +1,114 @@
-const productosEnCarrito=JSON.parse(localStorage.getItem("productos-en-carrito"))
-
+let productosEnCarrito=(localStorage.getItem("productos-en-carrito"))
+productosEnCarrito=JSON.parse(productosEnCarrito)
 const contenedorCarritoVacio=document.querySelector("#carrito-vacio")
 const contenedorCarritoProductos=document.querySelector("#carrito-productos")
 const contenedorCarritoAcciones=document.querySelector("#carrito-acciones")
 const contenedorCarritoComprado=document.querySelector("#carrito-comprado")
+let botonesEliminar=document.querySelectorAll(".carrito__productos-eliminar")
+const botonVaciar=document.querySelector(".carrito__acciones-vaciar")
+const botonComprar=document.querySelector(".carrito__acciones-comprar")
+const contenedorTotal=document.querySelector("#total")
 
-if(productosEnCarrito){
-    contenedorCarritoVacio.classList.add("disabled")
-    contenedorCarritoComprado.classList.add("disabled")
-    contenedorCarritoProductos.classList.remove("disabled")
-    contenedorCarritoAcciones.classList.remove("disabled")
+function cargarProductosCarrito (){
+    if(productosEnCarrito && productosEnCarrito.length>0){
+        contenedorCarritoVacio.classList.add("disabled")
+        contenedorCarritoComprado.classList.add("disabled")
+        contenedorCarritoProductos.classList.remove("disabled")
+        contenedorCarritoAcciones.classList.remove("disabled")
+        contenedorCarritoProductos.innerHTML=""
+        productosEnCarrito.forEach(producto =>{
+            const div=document.createElement("div")
+            div.classList.add("carrito__producto")
+            div.innerHTML=`
+            <img src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="carrito__producto-titulo">
+                <small>Titulo</small>
+                <h3>${producto.titulo}</h3>
+            </div>
+            <div class="carrito__producto-cantidad">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+            </div>
+            <div class="carrito__producto-precio">
+                <small>Precio</small>
+                <p>${producto.precio}</p>
+            </div>
+            <div class="carrito__producto-subtotal">
+                <small>Subtotal</small>
+                <p>${producto.precio * producto.cantidad}</p>
+            </div>
+            <button class="carrito__producto-eliminar" id="${producto.id}"><i class="bi bi-trash"></i></button>
+            `;
+            contenedorCarritoProductos.append(div)
+        })
+        actualizarBotonesEliminar()
+        actualiazarTotal()
+    }else{
+        contenedorCarritoVacio.classList.remove("disabled")
+        contenedorCarritoComprado.classList.add("disabled")
+        contenedorCarritoProductos.classList.add("disabled")
+        contenedorCarritoAcciones.classList.add("disabled")
+    }
+}
+cargarProductosCarrito ()
 
-   // contenedorCarritoProductos.innerHTML=""
-
-    productosEnCarrito.forEach(producto =>{
-        const div=document.createElement("div")
-        div.classList.add("carrito__producto")
-        div.innerHTML=`
-        <img src="${producto.imagen}" alt="${producto.titulo}">
-        <div class="carrito__producto-titulo">
-            <small>Titulo</small>
-            <h3>${producto.titulo}</h3>
-        </div>
-        <div class="carrito__producto-cantidad">
-            <small>Cantidad</small>
-            <p>${producto.cantidad}</p>
-        </div>
-        <div class="carrito__producto-precio">
-            <small>Precio</small>
-            <p>${producto.precio}</p>
-        </div>
-        <div class="carrito__producto-subtotal">
-            <small>Subtotal</small>
-            <p>${producto.precio * producto.cantidad}</p>
-        </div>
-        <button class="carrito__producto-eliminar" id="${producto.id}"><i class="bi bi-trash"></i></button>
-        `;
-        contenedorCarritoProductos.append(div)
-        
+function actualizarBotonesEliminar(){
+    botonesEliminar=document.querySelectorAll(".carrito__producto-eliminar")
+    botonesEliminar.forEach(boton =>{
+        boton.addEventListener("click", eliminarDelCarrito)
     })
+}
 
-}else{
-    contenedorCarritoVacio.classList.remove("disabled")
-    contenedorCarritoComprado.classList.add("disabled")
+function eliminarDelCarrito(e){
+    Toastify({
+        text: "Producto eliminado!",
+        duration: 700,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        //close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "rgb(183,209,163)",
+            background: "linear-gradient(90deg, rgba(183,209,163,1) 18%, rgba(252,247,211,1) 98%)",
+            color:"#9d5e27",
+            borderRadius:"15px",
+            border: "1px solid #dee8be",
+            textTransform:"uppercase",
+            fontSize: ".75rem"
+        },
+        offset: {
+            x: 30, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: 10 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+        onClick: function(){} // Callback after click
+    }).showToast();
+    const idBoton=e.currentTarget.id;
+    const index=productosEnCarrito.findIndex(producto=>producto.id===idBoton)
+    productosEnCarrito.splice(index, 1)
+    cargarProductosCarrito ()
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+}
+
+botonVaciar.addEventListener("click", vaciarCarrito)
+function vaciarCarrito(){
+    productosEnCarrito.length=0
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+    cargarProductosCarrito ()
+}
+
+function actualiazarTotal() {
+    const totalCalculado=productosEnCarrito.reduce((acc, producto) => acc+(producto.precio*producto.cantidad), 0)
+    total.innerText=`${totalCalculado}`
+}
+
+botonComprar.addEventListener("click",comprarCarrito)
+function comprarCarrito(){
+    productosEnCarrito.length=0
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+    contenedorCarritoVacio.classList.add("disabled")
+    contenedorCarritoComprado.classList.remove("disabled")
     contenedorCarritoProductos.classList.add("disabled")
     contenedorCarritoAcciones.classList.add("disabled")
 }
-
--- js/main.js
+++ js/main.js
@@ -1,115 +1,15 @@
-// PRODUCTOS
-const productos = [
-    // Abrigos
-    {
-        id: "producto-01",
-        titulo: "Producto 01",
-        imagen:  " ../img/figuras/goku/figura-goku3.jpeg",
-        categoria: {
-            nombre: "Dragon ball",
-            id: "figuraDragonBall",
-            id2: "figuras"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-02",
-        titulo: "Producto 02",
-        imagen: "../img/figuras/goku/figura-goku3.jpeg",
-        categoria: {
-            nombre: "Dragon ball",
-            id: "figuraDragonBall",
-            id2: "figuras"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-03",
-        titulo: "Producto 03",
-        imagen: "../img/figuras/goku/figura-goku3.jpeg",
-        categoria: {
-            nombre: "Naruto",
-            id: "figuraNaruto",
-            id2: "figuras"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-04",
-        titulo: "Producto 04",
-        imagen: "../img/figuras/goku/figura-goku3.jpeg",
-        categoria: {
-            nombre: "Naruto",
-            id: "figuraNaruto",
-            id2: "figuras"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-05",
-        titulo: "Producto 05",
-        imagen: "../img/mangas/seinen/seinen-macross.jpg",
-        categoria: {
-            nombre: "Shonen",
-            id: "mangaShonen",
-            id2: "mangas"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-06",
-        titulo: "Producto 06",
-        imagen: "../img/mangas/seinen/seinen-macross.jpg",
-        categoria: {
-            nombre: "Shonen",
-            id: "mangaShonen",
-            id2: "mangas"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-07",
-        titulo: "Producto 07",
-        imagen: "../img/mangas/seinen/seinen-macross.jpg",
-        categoria: {
-            nombre: "Seinen",
-            id: "mangaSeinen",
-            id2: "mangas"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-08",
-        titulo: "Producto 08",
-        imagen: "../img/mangas/seinen/seinen-macross.jpg",
-        categoria: {
-            nombre: "Seinen",
-            id: "mangaSeinen",
-            id2: "mangas"
-        },
-        precio: 1000
-    },
-    {
-        id: "producto-09",
-        titulo: "Producto 09",
-        imagen: "../img/mangas/seinen/seinen-macross.jpg",
-        categoria: {
-            nombre: "Seinen",
-            id: "mangaSeinen",
-            id2: "mangas"
-        },
-        precio: 1000
-    },
-    
-];
-
+let productos=[]
+fetch("./js/productos.json")
+    .then(response=>response.json())
+    .then(data=>{
+        productos=data
+        cargarProductos(productos)
+    })
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria")
 const tituloPrincipal = document.querySelector("#titulo-principal")
 let botonesAgregar=document.querySelectorAll(".producto__texto-boton")
 const numerito = document.querySelector("#numerito")
-
-
 
 function cargarProductos(productosElegidos){
     contenedorProductos.innerHTML=""
@@ -129,7 +29,6 @@ function cargarProductos(productosElegidos){
     actualizarBotonesAgregar()
 }
 
-cargarProductos(productos)
 
 botonesCategorias.forEach(boton=>{
     boton.addEventListener("click",(e)=> {
@@ -138,7 +37,6 @@ botonesCategorias.forEach(boton=>{
         if(e.currentTarget.id != "todos"){
             const productosCategoria=productos.find(producto=>producto.categoria.id===e.currentTarget.id)
             tituloPrincipal.innerHTML=productosCategoria.categoria.nombre
-
             const productosBoton=productos.filter(producto=>producto.categoria.id===e.currentTarget.id)
             cargarProductos(productosBoton);
         // }else if (e.currentTarget.id === productos.filter(producto=>producto.categoria.id2)){
@@ -161,18 +59,43 @@ function actualizarBotonesAgregar(){
 
 let productosEnCarrito
 
-const productosEnCarritoLS=JSON.parse(localStorage.getItem("productos-en-carrito"))
+let productosEnCarritoLS=localStorage.getItem("productos-en-carrito")
 if (productosEnCarritoLS) {
-    productosEnCarrito=productosEnCarritoLS
+    productosEnCarrito=JSON.parse(productosEnCarritoLS)
     actualizarNumerito()
 }else{
     productosEnCarrito=[]
 }
 
 function agregarAlCarrito(e){
+    Toastify({
+        text: "Producto agregado!",
+        duration: 700,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        //close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "rgb(183,209,163)",
+            background: "linear-gradient(90deg, rgba(183,209,163,1) 18%, rgba(252,247,211,1) 98%)",
+            color:"#9d5e27",
+            borderRadius:"15px",
+            border: "1px solid #dee8be",
+            textTransform:"uppercase",
+            fontSize: ".75rem"
+        },
+        offset: {
+            x: 30, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: 10 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+        onClick: function(){} // Callback after click
+    }).showToast();
+
+
     const idBoton=e.currentTarget.id
     const productoAgregado=productos.find(producto=>producto.id===idBoton)
-
     if (productosEnCarrito.some(producto=>producto.id===idBoton)){
         const index = productosEnCarrito.findIndex(producto=>producto.id===idBoton)
         productosEnCarrito[index].cantidad++
@@ -181,7 +104,6 @@ function agregarAlCarrito(e){
         productosEnCarrito.push(productoAgregado)
     }
     actualizarNumerito()
-
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
 }
 
